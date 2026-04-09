6f3563308efaec41d56a91be37e243a8c1a944f9
rakibulhasan02 <rakibul.ict02@gmail.com>
2026-04-10 01:26:11 +0600

    add another feature

--- script/index.js
+++ script/index.js
@@ -9,6 +9,13 @@ const loadLesson=()=>{
     })
 }
 
+const removeActive=()=>{
+    const lessonButtons=document.querySelectorAll(".lesson-btn");
+    lessonButtons.forEach(btn=>{
+        btn.classList.remove("active");
+    })
+}
+
 const loadLevelWord=(id)=>{
     // console.log(id);
     const url=`https://openapi.programming-hero.com/api/level/${id}`;
@@ -16,6 +23,10 @@ const loadLevelWord=(id)=>{
     .then(res=>res.json())
     .then(data=>{
         // console.log(data);
+        removeActive();// remove active class from all buttons
+        const clickBtn=document.getElementById(`lesson-btn-${id}`);
+        // console.log(clickBtn);
+        clickBtn.classList.add("active");// add active class to the clicked button
         displayLevelWord(data.data);
     })
 }
@@ -24,22 +35,35 @@ const displayLevelWord=(words)=>{
     // console.log(words);
     const wordContainer=document.getElementById('word-container');
     wordContainer.innerHTML="";
+
+    if(words.length==0){
+        wordContainer.innerHTML=`
+       
+        <div class="text-center col-span-full font-bangla space-y-4">
+         <span class="flex justify-center"><img src="./assets/alert-error.png"></span>
+        <p class="text-opacity-40">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="text-4xl font-bold text-opacity-20">নেক্সট Lesson এ যান</h2>
+    </div>
+        `;
+        return;
+        
+    }
     words.forEach(word=>{
         const levelWord=document.createElement("div");
         levelWord.innerHTML=`
         <div class="card bg-white p-5 rounded-lg text-center space-y-2">
-            <h2 class="font-bold text-xl">${word.word}</h2>
+            <h2 class="font-bold text-xl">${word.word ? word.word:"শব্দ পাওয়া যায় নি।"}</h2>
             <p>Meaning /Pronounciation</p>
-            <div class="font-semibold text-3xl opacity-[80%]">"${word.meaning} /${word.pronunciation}"</div>
+            <div class="font-semibold text-3xl opacity-[80%]">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি।"} /${word.pronunciation ? word.pronunciation:"pronounciation পাওয়া যায় নি।"}"</div>
             <div class="flex justify-between my-4 items-center">
-                <button class="btn "><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="my_modal_5.showModal()" class="btn "><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn "><i class="fa-solid fa-volume-high"></i></button>
               
             </div>
         </div>
         `
         wordContainer.append(levelWord);
-        console.log(word);
+        // console.log(word);
     })
 }
 const displayLesson=(lessons)=>{
@@ -56,7 +80,7 @@ const displayLesson=(lessons)=>{
    // 3.create Element
         const btnDiv=document.createElement('div');
         btnDiv.innerHTML=`
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-primary btn-outline"><i class="fa-solid fa-book-open"></i>lesson-${lesson.level_no}</button>
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-primary btn-outline lesson-btn"><i class="fa-solid fa-book-open"></i>lesson-${lesson.level_no}</button>
         `
  // 4.append the element
         // console.log(lesson);
